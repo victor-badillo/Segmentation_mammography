19bed731ac37c5987b1508fa8d36a579e2ac3f36
victor-badillo <victor.badillo@udc.es>
2024-12-15 23:03:05 +0100

    Missing flip and draw

--- src/main.py
+++ src/main.py
@@ -68,7 +68,7 @@ def perfil_muscle(without_muscle, mirrored):
     plt.show()
 '''
 
-
+'''
 def perfil_muscle(without_muscle, mirrored):
     
     # 1. Binarización
@@ -106,6 +106,44 @@ def perfil_muscle(without_muscle, mirrored):
     plt.subplot(2, 4, 7), plt.title("Resultado"), plt.imshow(image, cmap='gray')
     plt.subplot(2, 4, 8), plt.title("Resultado close"), plt.imshow(close_image, cmap='gray')
     plt.show()
+'''
+
+
+def perfil_muscle(without_muscle, mirrored):
+    
+    # 1. Binarización
+    _, binary = cv2.threshold(without_muscle, 1, 255, cv2.THRESH_BINARY)
+    binary = keep_largest_object(binary)
+
+    # 2. Apertura morfológica para eliminar ruido pequeño y separar posibles regiones
+    radius = 10  # Ajusta el radio según sea necesario
+    kernel_apertura = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2*radius+1, 2*radius+1))  # Ajusta el tamaño según el dataset
+    clean_smooth = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel_apertura)
+
+    # 3. Cerrar huecos (Closing)
+    radius = 30
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2*radius+1, 2*radius+1))
+    close = cv2.morphologyEx(clean_smooth, cv2.MORPH_CLOSE, kernel)
+
+    #Quedarse con al region mas grande de clean_smooth
+    largest_close = keep_largest_object(close)
+
+    close_image =  cv2.bitwise_and(without_muscle, largest_close)
+
+
+    if mirrored == True:
+        close_image = cv2.flip(close_image, 1)
+
+    plt.figure(figsize=(20, 20))
+    plt.subplot(2, 4, 1), plt.title("Original"), plt.imshow(without_muscle, cmap='gray')
+    plt.subplot(2, 4, 2), plt.title("Binary"), plt.imshow(binary, cmap='gray')
+    plt.subplot(2, 4, 3), plt.title("Opening"), plt.imshow(clean_smooth, cmap='gray')
+    plt.subplot(2, 4, 4), plt.title("Close"), plt.imshow(close, cmap='gray')
+    plt.subplot(2, 4, 5), plt.title("Mas grande close"), plt.imshow(largest_close, cmap='gray')
+    plt.subplot(2, 4, 6), plt.title("Resultado close"), plt.imshow(close_image, cmap='gray')
+    plt.show()
+
+    return close_image, largest_close
 
 
 
@@ -133,8 +171,36 @@ def process_images_in_directory(directory_path):
             
             without_muscle = substract_muscle(without_labels, breast_filter, breast_gauss2)
 
-            without_muscle_smooth = perfil_muscle(without_muscle, mirrored)
+            without_muscle_smooth, bin_contour = perfil_muscle(without_muscle, mirrored)
 
+            surrounded_breast, _ = cv2.findContours(bin_contour, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            largest_contour = max(surrounded_breast, key=cv2.contourArea)
+            #contoured_image = np.zeros_like(without_labels)
+            #cv2.drawContours(contoured_image, [largest_contour], -1, 255, 2)
+
+
+
+            contoured_image  = cv2.cvtColor(imagen, cv2.COLOR_GRAY2BGR)
+            cv2.drawContours(contoured_image , [largest_contour], -1, (255, 0, 0), 2) 
+
+
+
+            plt.figure(figsize=(20, 20))
+            plt.subplot(2, 4, 1), plt.title("Original"), plt.imshow(imagen, cmap='gray')
+            plt.subplot(2, 4, 2), plt.title("Sin etiquetas"), plt.imshow(without_labels, cmap='gray')
+            plt.subplot(2, 4, 3), plt.title("Orientada"), plt.imshow(breast_gauss2, cmap='gray')
+            plt.subplot(2, 4, 4), plt.title("Filtro"), plt.imshow(breast_filter, cmap='gray')
+            plt.subplot(2, 4, 5), plt.title("Sin musculo"), plt.imshow(without_muscle, cmap='gray')
+            plt.subplot(2, 4, 6), plt.title("Sin musculo smooth"), plt.imshow(without_muscle_smooth, cmap='gray')
+            plt.subplot(2, 4, 7), plt.title("Binaria"), plt.imshow(bin_contour, cmap='gray')
+            plt.subplot(2, 4, 8), plt.title("Contorno"), plt.imshow(contoured_image, cmap='gray')
+            plt.show()
+
+            plt.figure(figsize=(20, 20))
+            plt.subplot(1, 3, 1), plt.title("Original"), plt.imshow(imagen, cmap='gray')
+            plt.subplot(1, 3, 2), plt.title("Binaria"), plt.imshow(bin_contour, cmap='gray')
+            plt.subplot(1, 3, 3), plt.title("Contorno"), plt.imshow(contoured_image, cmap='gray')
+            plt.show()
 
 
 
@@ -473,12 +539,12 @@ def substract_muscle(without_labels, breast, original):
 
 if __name__ == "__main__":
 
-    imagen_path = "data/Glandular-graso/mdb072.jpg"
+    # imagen_path = "data/Glandular-graso/mdb072.jpg"
 
-    image = cv2.imread(imagen_path, cv2.IMREAD_GRAYSCALE)
+    # image = cv2.imread(imagen_path, cv2.IMREAD_GRAYSCALE)
 
-    if image is None:
-        raise FileNotFoundError(f"No se pudo cargar la imagen en la ruta: {imagen_path}")
+    # if image is None:
+    #     raise FileNotFoundError(f"No se pudo cargar la imagen en la ruta: {imagen_path}")
     
 
     # without_labels = pre_process(image)
@@ -492,8 +558,6 @@ if __name__ == "__main__":
     # without_muscle = substract_muscle(without_labels, breast_filter, breast_gauss2)
 
     # without_muscle_smooth = perfil_muscle(without_muscle, mirrored)
-    
-
     process_all_directories()
 
 
